e5d7496fbc0a416b876361e8f7ada6ab6db25be0
viviennelin123 <viviennelin@berkeley.edu>
2021-03-21 17:37:03 -0700

    Switching camera
    -press F to switch between cameras
    -added transition between camera switching
    -this is in the "vivienne" scene

---
+++ CWG Sp21 Game Jam/Assets/Scripts/CamSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+    public Camera cam1; 
+    public Camera cam2;
+ 
+    void Start()
+    {
+        Camera[] cams = FindObjectsOfType<Camera>();
+        cam1 = cams[0];
+        cam2 = cams[1];
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            cam1.enabled = !cam1.enabled;
+            cam2.enabled = !cam2.enabled;
+        }
+    }
+}
