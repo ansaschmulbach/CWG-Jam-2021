bca07bd1e51f01ff5c3cc56821ff58addc1b2300
Viansa Schmulbach <ansa@berkeley.edu>
2021-03-22 07:43:43 -0700

    add walking animations

--- CWG Sp21 Game Jam/Assets/Movement/PlayerMovement.cs
+++ CWG Sp21 Game Jam/Assets/Movement/PlayerMovement.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -12,15 +14,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float m_gravity;
     [SerializeField] private float m_jumpPow;
     [SerializeField] private float yScaleAmt;
-    [SerializeField] private float m_jumpHeight;
-    [SerializeField] private float m_yDir;
-    
+
     #endregion
 
     #region Cached Variables
 
     private Rigidbody2D cr_rb;
-
+    private Animator animator;
+    private Vector3 scale;
+    
     #endregion
 
     #region Public Vars
@@ -36,22 +38,22 @@ public class PlayerMovement : MonoBehaviour
 
     private float yDelta;
     private float preDeltaY;
-    private int debugCount;
 
     #endregion
-    
+
+    #region Instantiation Method
+
     void Start()
     {
         cr_rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         touchingGround = true;
         isJumping = false;
-        // m_speed = 150f;
-        // m_gravity = 12;
-        // m_jumpSpeed = 300;
-        // yScaleAmt = 0.015f;
+        scale = transform.localScale;
     }
 
-    // Update is called once per frame
+    #endregion
+    
     void Update()
     {
         float xMov = Input.GetAxis("Horizontal");
@@ -60,57 +62,126 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 vel = Vector3.right * (xMov * m_speed);
 
-        if (!space && !isJumping)
+        if (space && !isJumping)
+        {
+            StartJump(vel);
+        } 
+        else if (!isJumping)
         {
             vel += Vector3.up * (yMov * m_speed);
             if (!touchingGround) {
                 //Destroy(this.gameObject);
             }
+            this.cr_rb.velocity = vel;
         } 
-        else if (!isJumping && space)
+        else
         {
-            vel += Vector3.up * m_jumpPow;
-            isJumping = true;
-            yDelta = 0;
-            originalY = this.transform.position.y;
-            cr_rb.velocity = vel;
-            debugCount = 0;
-            preDeltaY = vel.y;
-            this.transform.position += Vector3.up * 0.00001f;
-            Debug.Log(cr_rb.velocity.y);
-            return;
+            JumpContinue(vel, yMov);
         }
         
-        if (isJumping)
-        {
-            debugCount++;
-            preDeltaY -= (m_gravity * Time.deltaTime);
-            vel.y = preDeltaY;
-            yDelta += CalcYDelta(yMov) * Time.deltaTime;
-            vel += Vector3.up * CalcYDelta(yMov);
-
-            cr_rb.velocity = vel;
-            
-            float currYDelta = this.transform.position.y - originalY;
-            //Debug.Log(yDelta + " " + currYDelta);
-            Debug.Log(CalcYDelta(yMov) + " " + vel.y + " " + preDeltaY);
-            Debug.Log(currYDelta + " " + yDelta);
-            if (currYDelta <= yDelta && preDeltaY < 0)
-            {
-                //Debug.Log(currYDelta + " " + yDelta );
-                vel = Vector3.zero;
-                isJumping = false;
-            }
-        }
-
-        cr_rb.velocity = vel;
+        UpdateAnimator(xMov, yMov);
 
     }
 
+    #region Jump Methods
+
+    void StartJump(Vector3 vel)
+    {
+        vel += Vector3.up * m_jumpPow;
+        isJumping = true;
+        yDelta = 0;
+        originalY = this.transform.position.y;
+        cr_rb.velocity = vel;
+        preDeltaY = vel.y;
+        this.transform.position += Vector3.up * 0.00001f;
+    }
+
+    void JumpContinue(Vector3 vel, float yMov)
+    {
+        preDeltaY -= (m_gravity * Time.deltaTime);
+        vel.y = preDeltaY;
+        yDelta += CalcYDelta(yMov) * Time.deltaTime;
+        vel += Vector3.up * CalcYDelta(yMov);
+        cr_rb.velocity = vel;
+        float currYDelta = this.transform.position.y - originalY;
+        if (currYDelta <= yDelta && preDeltaY < 0)
+        {
+            vel = Vector3.zero;
+            isJumping = false;
+        }
+        cr_rb.velocity = vel;
+    }
+    
+    
     float CalcYDelta(float yMov)
     {
         return yMov * m_speed * yScaleAmt;
     }
+
+    #endregion
+
+    #region Animator
+
+    int AnimatorState(float xMov, float yMov) 
+    {
+        Debug.Log(yMov - Math.Abs(xMov * 2));
+        if (isJumping)
+        {
+            return 0;
+        }
+        if (Math.Abs(xMov) - Math.Abs(yMov) > 0.2)
+        {
+            if (xMov > 0)
+            {
+                this.transform.localScale = new Vector3(-scale.x, scale.y, scale.z);
+            }
+            else
+            {
+                this.transform.localScale = scale;
+            }
+            return 2;
+        }
+        if (yMov > 0)
+        {
+            return 3;
+        }
+
+        return 1;
+    }
+
+    void UpdateAnimator(float xMov, float yMov)
+    {
+        int anim = AnimatorState(xMov, yMov);
+        switch (anim)
+        {
+            case 0:
+                animator.SetBool("jump", true);
+                animator.SetBool("front", false);
+                animator.SetBool("side walk", false);
+                animator.SetBool("back", false);
+                break;
+            case 1:
+                animator.SetBool("jump", false);
+                animator.SetBool("front", true);
+                animator.SetBool("side walk", false);
+                animator.SetBool("back", false);
+                break;
+            case 2:
+                animator.SetBool("jump", false);
+                animator.SetBool("front", false);
+                animator.SetBool("side walk", true);
+                animator.SetBool("back", false);
+                break;
+            case 3:
+                animator.SetBool("jump", false);
+                animator.SetBool("front", false);
+                animator.SetBool("side walk", false);
+                animator.SetBool("back", true);
+                break;
+        }
+    }
+
+    #endregion
     
-    
+
 }
