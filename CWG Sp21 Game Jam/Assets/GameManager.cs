6e2322c0342c9937e5db7d74c8d887a772cdb84a
Viansa Schmulbach <ansa@berkeley.edu>
2021-03-22 09:22:13 -0700

    add gamemanager

---
+++ CWG Sp21 Game Jam/Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    void LoadMainMenu()
+    {
+        
+    }
+
+    void LoadGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    void LoadDeathScene()
+    {
+        
+    }
+    
+}
